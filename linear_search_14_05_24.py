ec56911f8f528edd135906b39b5ccf938b5b9a45
Udit Vikhe <udit.vikhe@gmail.com>
2024-05-14 17:38:46 +0530

    linear search practise day 2

---
+++ linear_search_14_05_24.py
@@ -0,0 +1,17 @@
+def linear_search(arr, N, x):
+    for i in range(0, N):
+        if arr[i] == x:
+            return i
+    return -1
+
+if __name__ == "__main__":
+    arr = [2,3,4,10,40]
+    N = len(arr)
+    x = 11
+
+    result = linear_search(arr, N, x)
+
+    if result == -1:
+        print(f'{x} not present in the given array')
+    else:
+        print(f'{x} is present at index {result}')
