0dafa5f245be1b832833fb812eb913a08f228ae5
Udit Vikhe <udit.vikhe@gmail.com>
2024-05-15 06:10:05 +0530

    linear search day 3 and binary search day 2

---
+++ linear_search_15_05_24.py
@@ -0,0 +1,16 @@
+def linear_search(arr, N, x):
+    for i in range(0,N):
+        if arr[i] == x:
+            return i
+    return -1
+
+if __name__ == "__main__":
+    arr = [2, 3, 4, 10, 40]
+    x = 10
+    N = len(arr)
+
+    result = linear_search(arr, N, x)
+    if result == -1:
+        print(f'{x} not present in the given array')
+    else:
+        print(f'{x} is present at index {result}')
